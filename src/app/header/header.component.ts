2b8b83bd90273fb1727b46ebe7f01ba308f3cf20
Nicolas Macias Duque <nicolas.macias23211@elpoli.edu.co>
2025-02-22 23:03:16 -0500

    "Se inicia con todo el front de nuevo"

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Inject } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+constructor(public router: Router) { }
 
 }
+
